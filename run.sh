cfb4e1fcc26c347ce783576e41b4b5b0193663c0
Pi-Yueh Chuang <pychuang@gwu.edu>
2019-12-02 21:05:32 -0500

    further minize image size

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@
 #
 # Credit: https://github.com/moby/moby/blob/master/contrib/mkimage-arch.sh
 #
-# Require arch-install-scripts from Arch Linux
+# Require arch-install-scripts from Arch Linux on the host
+
+# stop script when error happens
+set -e
 
 # version/date
 DATE="2016.08.01"
@@ -17,15 +20,19 @@ REPOURL="$BASEURL/repos/$(echo $DATE | sed 's/\./\//g')"
 # filename
 BOOTSTRAP="archlinux-bootstrap-$DATE-x86_64.tar.gz"
 
-# some alias
-ROOTFS="root.x86_64"
-CHROOT="$ROOTFS/bin/arch-chroot $ROOTFS"
+# some alias; we use host's arch-chroot, instead of chroot's arch-chroot
+ROOTFS="mnt"
+CHROOT="arch-chroot $ROOTFS"
 
-# packages that must present in chroot
-PKGREQUIRED=(
-    bash zsh base-devel haveged pacman pacman-mirrorlist vim bzip2 coreutils 
-    device-mapper wget curl)
+# some core utilities beside bash pacman coreutils findutils util-linux glibc
+PKGREQUIRED=(tar less diffutils grep sed gawk procps-ng binutils gzip which)
 PKGREQUIRED=${PKGREQUIRED[*]}
+
+# packages that will be removed later
+PKGREMOVE=(
+    arch-install-scripts systemd iptables libmnl libnftnl dbus libdbus kbd 
+    kmod libelf libseccomp hwids)
+PKGREMOVE=${PKGREMOVE[*]}
 
 # download the bootstrap and signature for verification
 wget -c $ISOURL/$BOOTSTRAP
@@ -36,6 +43,12 @@ pacman-key --verify $BOOTSTRAP.sig
 
 # Extract
 tar -xf $BOOTSTRAP
+
+# bind to a mountpoint so we can use host's arch-chroot
+if [ ! -d "$ROOTFS" ]; then
+    mkdir $ROOTFS
+fi
+mount --bind root.x86_64 $ROOTFS
 
 # fix the repository mirror to a fixed-date snapshot.
 echo "Server = $REPOURL/\$repo/os/\$arch" > $ROOTFS/etc/pacman.d/mirrorlist
@@ -52,6 +65,17 @@ $CHROOT rm -r /root/keys
 # install basic packages
 $CHROOT pacman -S --noconfirm $PKGREQUIRED
 
+# locale: en_US.UTF-8
+echo "en_US.UTF-8 UTF-8" > $ROOTFS/etc/locale.gen
+$CHROOT locale-gen
+echo "LANG=en_US.UTF-8" > $ROOTFS/etc/locale.conf
+
+# timezone: always UTC
+ln -sf $ROOTFS/usr/share/zoneinfo/UTC $ROOTFS/etc/localtime
+
+# remove unnecessary packages
+$CHROOT pacman -Rnu --noconfirm $PKGREMOVE
+
 # clean tarbals to save space
 expect <<EOF
 spawn $CHROOT pacman -Scc
@@ -62,25 +86,40 @@ send -- "y\r"
 expect eof
 EOF
 
+# clean man pages and non-en locale files
+rm -r $ROOTFS/usr/share/man/*
+rm -r $ROOTFS/usr/share/doc/*
+rm -r $ROOTFS/usr/share/info/*
+mv $ROOTFS/usr/share/locale/en_US $ROOTFS/root
+mv $ROOTFS/usr/share/locale/locale.alias $ROOTFS/root
+rm -r $ROOTFS/usr/share/locale/*
+mv $ROOTFS/root/* $ROOTFS/usr/share/locale
+mv $ROOTFS/usr/share/zoneinfo/UTC $ROOTFS/root
+rm -r $ROOTFS/usr/share/zoneinfo/*
+mv $ROOTFS/root/UTC $ROOTFS/usr/share/zoneinfo
+
+# umount the mountpoint; after this point, we can not use $CHROOT anymore
+umount -r $ROOTFS
+
 # udev doesnt work in containers, rebuild /dev
-rm -rf $ROOTFS/dev
-mkdir -p  $ROOTFS/dev
-mknod -m 666 $ROOTFS/dev/null c 1 3
-mknod -m 666 $ROOTFS/dev/zero c 1 5
-mknod -m 666 $ROOTFS/dev/random c 1 8
-mknod -m 666 $ROOTFS/dev/urandom c 1 9
-mkdir -m 755 $ROOTFS/dev/pts
-mkdir -m 1777 $ROOTFS/dev/shm
-mknod -m 666 $ROOTFS/dev/tty c 5 0
-mknod -m 600 $ROOTFS/dev/console c 5 1
-mknod -m 666 $ROOTFS/dev/tty0 c 4 0
-mknod -m 666 $ROOTFS/dev/full c 1 7
-mknod -m 600 $ROOTFS/dev/initctl p
-mknod -m 666 $ROOTFS/dev/ptmx c 5 2
-ln -sf /proc/self/fd $ROOTFS/dev/fd
+rm -rf root.x86_64/dev
+mkdir -p  root.x86_64/dev
+mknod -m 666 root.x86_64/dev/null c 1 3
+mknod -m 666 root.x86_64/dev/zero c 1 5
+mknod -m 666 root.x86_64/dev/random c 1 8
+mknod -m 666 root.x86_64/dev/urandom c 1 9
+mkdir -m 755 root.x86_64/dev/pts
+mkdir -m 1777 root.x86_64/dev/shm
+mknod -m 666 root.x86_64/dev/tty c 5 0
+mknod -m 600 root.x86_64/dev/console c 5 1
+mknod -m 666 root.x86_64/dev/tty0 c 4 0
+mknod -m 666 root.x86_64/dev/full c 1 7
+mknod -m 600 root.x86_64/dev/initctl p
+mknod -m 666 root.x86_64/dev/ptmx c 5 2
+ln -sf /proc/self/fd root.x86_64/dev/fd
 
 # make a docker image locally
-tar --numeric-owner --xattrs --acls -C $ROOTFS -c .  | docker import - pychuang/oldarch:$DATE
+tar --numeric-owner --xattrs --acls -C root.x86_64 -c . | docker import - pychuang/oldarch:$DATE
 
 # return a success message from inside Docker container
 docker run --rm=true -t pychuang/oldarch:$DATE echo Success.
@@ -89,3 +128,4 @@ docker run --rm=true -t pychuang/oldarch:$DATE echo Success.
 rm $BOOTSTRAP
 rm $BOOTSTRAP.sig
 rm -rf $ROOTFS
+rm -rf root.x86_64
